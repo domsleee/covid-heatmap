09a19229e17c415e25f801e1c0d102389859c62f
Dom Slee <domslee1@gmail.com>
2020-10-18 19:23:02 +1100

    Improve text changing behaviour

--- src/components/go.js
+++ src/components/go.js
@@ -8,6 +8,15 @@ document.body.appendChild(myEl);
 const MAX_CASES = 42;
 const HEATMAP_DATA_BASEURL = "https://domsleee.github.io/covid-heatmap-data";
 const GEOJSON_URL = (isDevelopmentEnv ? BASEPATH + '/sample' : HEATMAP_DATA_BASEURL) + '/suburb-10-nsw-proc.geojson';
+let textChangeableState = {
+  mapMouseDown: false,
+  temporarilyOff: false
+};
+
+function isTextChangeable() {
+  return !textChangeableState.mapMouseDown && !textChangeableState.temporarilyOff;
+}
+
 
 function heatMapColorforValue(value) {
   if (value / MAX_CASES > 1.0) {
@@ -45,12 +54,25 @@ export async function go() {
   })
 
   map.data.addListener('mousemove', function(event) {
+   // console.log('mousemove');
     const properties = event.feature.j;
     myEl.style.display = "block";
-    myEl.innerHTML = `Postcode: ${properties.postCode}<br />Cases: ${properties.cases}<br />Suburbs: ${properties.suburb}`;
+    if (isTextChangeable()) myEl.innerHTML = `Postcode: ${properties.postCode}<br />Cases: ${properties.cases}<br />Suburbs: ${properties.suburb}`;
   });
   
   map.data.addListener('mouseout', function() {
-    //map.data.revertStyle();
+    //console.log('mouseout', event);
+    if (isTextChangeable()) myEl.innerHTML = ``;
   });
+
+  map.data.addListener('mousedown', () => {
+    //console.log("map mouse down...");
+    textChangeableState.mapMouseDown = true;
+  })
+  map.data.addListener('mouseup', () => {
+    //console.log("map mouse up...");
+    textChangeableState.mapMouseDown = false;
+    textChangeableState.temporarilyOff = true;
+    setTimeout(() => textChangeableState.temporarilyOff = false, 50);
+  })
 }
