e2bdef6203adc24c76920ce2ea71cd4adec44867
Dom Slee <domslee97@gmail.com>
2021-05-01 15:40:15 +1000

    function order

--- src/components/go.js
+++ src/components/go.js
@@ -5,20 +5,6 @@ import { setupFloatingEl } from './FloatingEl/FloatingEl';
 const HEATMAP_DATA_BASEURL = "https://domsleee.github.io/covid-heatmap-data";
 const GEOJSON_URL = (isDevelopmentEnv ? BASEPATH + '/proc' : HEATMAP_DATA_BASEURL) + '/suburb-10-nsw-proc.geojson';
 
-function heatMapColorforValue(value) {
-  if (value / LegendDefs.MAX_CASES > 1.0) {
-    console.log(`WARNING: exceeds max gradient ${value}`);
-  }
-  const normalized = Math.min(1.0, value / LegendDefs.MAX_CASES);
-  const h = (1.0 - normalized) * 240
-  "hsl(" + h + ", 100%, 50%)";
-
-  // https://colorbrewer2.org/#type=sequential&scheme=YlOrRd&n=9
-  const grad = LegendDefs.LEGEND_GRADIENT;
-  const ind = Math.min(Math.floor((grad.length * value)/LegendDefs.MAX_CASES), grad.length-1);
-  return grad[ind];
-}
-
 export async function go() {
   var map = new google.maps.Map(
     document.getElementById('map'), {
@@ -39,4 +25,18 @@ export async function go() {
   });
 
   setupFloatingEl(map);
+}
+
+function heatMapColorforValue(value) {
+  if (value / LegendDefs.MAX_CASES > 1.0) {
+    console.log(`WARNING: exceeds max gradient ${value}`);
+  }
+  const normalized = Math.min(1.0, value / LegendDefs.MAX_CASES);
+  const h = (1.0 - normalized) * 240
+  "hsl(" + h + ", 100%, 50%)";
+
+  // https://colorbrewer2.org/#type=sequential&scheme=YlOrRd&n=9
+  const grad = LegendDefs.LEGEND_GRADIENT;
+  const ind = Math.min(Math.floor((grad.length * value)/LegendDefs.MAX_CASES), grad.length-1);
+  return grad[ind];
 }
